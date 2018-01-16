6eeea73760f114c1f8616da7ab60ef85cfbf3057
Clay190 <31509660+Clay190@users.noreply.github.com>
2018-01-16 14:47:54 -0500

    Updated from Brython Server: 1/16/2018 2:47:53 PM

--- distanceFormula.py
+++ distanceFormula.py
@@ -1,1 +1,12 @@
+#Clay Kynor and Courntey Tally
+#1.16.18
+#distance.py - calculates distance between two points
 
+from math import sqrt
+
+x1 = float(input("Enter x1 "))
+y1 = float(input("Enter y1 "))
+x2 = float(input("Enter x2 "))
+y2 = float(input("Enter y2 "))
+
+print(sqrt((x2-x1)**2 + (y2-y1)**2))
